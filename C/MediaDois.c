e6a9c7d3f5f22ad8bcc7fffc6ae087bcc13275be
Gabriel Nunes <47859373+Nuneszx@users.noreply.github.com>
2019-08-24 15:30:03 -0300

    Create MediaDois.c

---
+++ C/MediaDois.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  float A, B,C;
+    
+    scanf("%f" "%f" "%f", &A,&B,&C);
+
+  float MEDIA = ( A * 2 + B * 3 + C * 5 ) / (2+3+5);
+    
+    printf ("MEDIA = %.1f\n",MEDIA);
+    
+  return 0;
+
+}
