fbd1559c3d3ab789c43f94493725b2dfc7f88c9b
Gabriel Nunes <47859373+Nuneszx@users.noreply.github.com>
2019-08-24 15:34:48 -0300

    Create Esfera.c

---
+++ C/Esfera.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  double a, b;
+    scanf ("%lf", &a);
+
+    b = (4.0/3) * 3.14159 * a * a * a;
+
+    printf("VOLUME = %.3lf\n", b);
+  
+  return 0;
+}
