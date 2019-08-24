016964e4c3c981b8c8ef47a3491d5ad372bbc48e
Gabriel Nunes <47859373+Nuneszx@users.noreply.github.com>
2019-08-24 15:29:24 -0300

    Create  MediaUm.c

---
+++ C/MediaUm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  float A, B;
+    
+    scanf("%f" "%f", &A,&B);
+
+  float MEDIA = ( A * 3.5 + B * 7.5)/(3.5+7.5);
+    
+    printf ("MEDIA = %.5f\n",MEDIA);
+    
+  return 0;
+
+}
