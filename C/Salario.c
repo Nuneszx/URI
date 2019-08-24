c6d580d7348ded15846d1e5eae70acb88ef33124
Gabriel Nunes <47859373+Nuneszx@users.noreply.github.com>
2019-08-24 15:31:30 -0300

    Create Salario.c

---
+++ C/Salario.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int A, B;
+  float C,D;
+
+  scanf("%d" "%d" "%f", &A, &B, &C);
+
+  D = B * C;
+  
+  printf("NUMBER = %.d\n", A);
+  printf("SALARY = U$ %.2f\n", D);
+
+return 0;
+}
